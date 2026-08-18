69342a763dbe918bece6131779a813af12c45fe3
Vivek <268113948+VivekVRobotics@users.noreply.github.com>
2026-08-18 18:59:05 +0530

    Expose advanced robotics math, IK, energy, and safety APIs

--- src/robotics_control_lab/__init__.py
+++ src/robotics_control_lab/__init__.py
@@ -2,23 +2,35 @@
 
 from .controllers import ComputedTorqueController, JointPD
 from .dynamics import TwoRParameters, TwoRRobot
+from .energy import kinetic_energy, potential_energy, total_energy
+from .ik import IKResult, cartesian_velocity_control, damped_least_squares_ik
 from .jacobian import is_singular, jacobian, manipulability
 from .pid import PIDController
 from .planar_arm import Planar2R
+from .safety import JointLimits, rate_limit
 from .simulation import SimulationResult, simulate
-from .trajectory import cubic_interpolation
+from .trajectory import cubic_interpolation, quintic_interpolation
 
 __all__ = [
     "ComputedTorqueController",
     "JointPD",
     "TwoRParameters",
     "TwoRRobot",
+    "kinetic_energy",
+    "potential_energy",
+    "total_energy",
+    "IKResult",
+    "cartesian_velocity_control",
+    "damped_least_squares_ik",
     "is_singular",
     "jacobian",
     "manipulability",
     "PIDController",
     "Planar2R",
+    "JointLimits",
+    "rate_limit",
     "SimulationResult",
     "simulate",
     "cubic_interpolation",
+    "quintic_interpolation",
 ]
