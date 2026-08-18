59743ad467b6c28830d0370e06e199210a91634b
Vivek <268113948+VivekVRobotics@users.noreply.github.com>
2026-08-18 19:02:36 +0530

    Expose advanced robotics stack through public API

--- src/robotics_control_lab/__init__.py
+++ src/robotics_control_lab/__init__.py
@@ -3,34 +3,35 @@
 from .controllers import ComputedTorqueController, JointPD
 from .dynamics import TwoRParameters, TwoRRobot
 from .energy import kinetic_energy, potential_energy, total_energy
+from .estimation import KalmanState, LinearKalmanFilter, complementary_fusion
+from .hardware import HardwareInterface, JointState, SimulatedHardware, run_hil_cycle
+from .identification import FirstOrderFit, fit_first_order_step
 from .ik import IKResult, cartesian_velocity_control, damped_least_squares_ik
 from .jacobian import is_singular, jacobian, manipulability
+from .manipulator3d import Serial3R
+from .mpc import MPCResult, linear_mpc
+from .observers import momentum_disturbance_estimate, residual_gate
+from .optimization import project_box_qp, smooth_path
 from .pid import PIDController
+from .planning import CircleObstacle, astar, edge_collision_free, rrt
 from .planar_arm import Planar2R
 from .safety import JointLimits, rate_limit
+from .se3 import adjoint, pose_error, se3, se3_exp, so3_exp, so3_log, skew, twist_hat, twist_vee
 from .simulation import SimulationResult, simulate
 from .trajectory import cubic_interpolation, quintic_interpolation
 
 __all__ = [
-    "ComputedTorqueController",
-    "JointPD",
-    "TwoRParameters",
-    "TwoRRobot",
-    "kinetic_energy",
-    "potential_energy",
-    "total_energy",
-    "IKResult",
-    "cartesian_velocity_control",
-    "damped_least_squares_ik",
-    "is_singular",
-    "jacobian",
-    "manipulability",
-    "PIDController",
-    "Planar2R",
-    "JointLimits",
-    "rate_limit",
-    "SimulationResult",
-    "simulate",
-    "cubic_interpolation",
-    "quintic_interpolation",
+    "ComputedTorqueController", "JointPD", "TwoRParameters", "TwoRRobot",
+    "kinetic_energy", "potential_energy", "total_energy",
+    "KalmanState", "LinearKalmanFilter", "complementary_fusion",
+    "HardwareInterface", "JointState", "SimulatedHardware", "run_hil_cycle",
+    "FirstOrderFit", "fit_first_order_step",
+    "IKResult", "cartesian_velocity_control", "damped_least_squares_ik",
+    "is_singular", "jacobian", "manipulability", "Serial3R",
+    "MPCResult", "linear_mpc", "momentum_disturbance_estimate", "residual_gate",
+    "project_box_qp", "smooth_path", "PIDController",
+    "CircleObstacle", "astar", "edge_collision_free", "rrt", "Planar2R",
+    "JointLimits", "rate_limit",
+    "adjoint", "pose_error", "se3", "se3_exp", "so3_exp", "so3_log", "skew", "twist_hat", "twist_vee",
+    "SimulationResult", "simulate", "cubic_interpolation", "quintic_interpolation",
 ]
